f4fce94f6ea74d5a8a4a56ec9b2f07ca58b56f17
heouii <138588606+heouii@users.noreply.github.com>
2024-11-11 23:14:38 +0100

    base

---
+++ src/Struc.h
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct tree 
+{
+    int value;
+  struct Mail = *Mail
+
+    }tree;
+
+
+tree *new_tree(int x);
+
+    
---
+++ src/tree.h
@@ -0,0 +1,23 @@
+#include "Struct.h"
+
+
+
+tree *newtree(int x)
+
+{
+
+    tree *tr = malloc(sizeof(*tr));
+
+    if(tr == Null){
+
+        fprintf("L'allocation de l'arbre ne c'est pas faite.\n");
+
+        exit(EXIT_FAILURE);
+
+    
+    }
+
+
+    free(tree);
+}
+
